3337b692287430c45ef2c36579c983e717ee7520
Nick Ramsey <nramseysc@gmail.com>
2019-09-29 20:26:22 -0700

    add store and product models

--- db/migrate/20190930031931_create_products.rb
+++ db/migrate/20190930031931_create_products.rb
@@ -4,6 +4,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
       t.string :name
       t.text :ingredients
       t.integer :cost
+      t.integer :store_id
       t.timestamps
     end
   end
