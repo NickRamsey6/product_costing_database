9168511c5038e0097d471d4c3d3f55b52c1cf775
Nick Ramsey <nramseysc@gmail.com>
2019-09-29 22:19:15 -0700

    fix missing db pizza

--- db/migrate/20190927222038_create_stores.rb
+++ db/migrate/20190927222038_create_stores.rb
@@ -2,25 +2,26 @@ class CreateStores < ActiveRecord::Migration[6.0]
   def change
     create_table :stores do |t|
       t.string :name
-      t.integer :dailysales
+      t.integer :daily_sales
       t.integer :labor
       t.integer :cheese
       t.integer :pepperoni
-      t.integer :meatlover
-      t.integer :bbqchicken
-      t.integer :carneamante
-      t.integer :buffalochicken
-      t.integer :italiansausage
+      t.integer :meat_lover
+      t.integer :bbq_chicken
+      t.integer :carne_amante
+      t.integer :buffalo_chicken
+      t.integer :chicken_bacon_ranch
+      t.integer :italian_sausage
       t.integer :hawaiian
-      t.integer :garlicvegetarian
-      t.integer :chickenpesto
+      t.integer :garlic_vegetarian
+      t.integer :chicken_pesto
       t.integer :mediterranean
-      t.integer :tuscanveggie
+      t.integer :tuscan_veggie
       t.integer :margherita
-      t.integer :primerib
-      t.integer :thaichicken
-      t.integer :gryo
-      t.integer :coconutshrimp
+      t.integer :prime_rib
+      t.integer :thai_chicken
+      t.integer :gyro
+      t.integer :coconut_shrimp
 
       t.timestamps
     end
