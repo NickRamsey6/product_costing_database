b1de53bc1949b73cf5e17dd168faf80fd71f67d8
Nick Ramsey <nramseysc@gmail.com>
2019-10-01 15:26:43 -0700

    day identifier to prevent duplicate uploads

--- app/models/master.rb
+++ app/models/master.rb
@@ -2,7 +2,9 @@ require 'pg'
 class Master < ApplicationRecord
     def self.import(file)
       CSV.foreach(file.path, headers: true) do |row|
-        Master.create! row.to_hash
+        master = find_by_day(row["day"]) || new
+        master.attributes = row.to_hash
+        master.save!
       end
     end
 end
