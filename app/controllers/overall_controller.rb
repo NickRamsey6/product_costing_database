e3e82c335a05e3029e856d97c8784fb5ec2c810c
Nick Ramsey <nramseysc@gmail.com>
2019-10-01 10:58:52 -0700

    pre merge commit

--- app/controllers/overall_controller.rb
+++ app/controllers/overall_controller.rb
@@ -3,6 +3,8 @@ class OverallController < ApplicationController
   DB = PG.connect({:dbname => 'tuscan_stone_development'})
   def index
     @overall_sales = ActiveRecord::Base.connection.execute("SELECT SUM(sales) FROM sales;").values
+    @overall_labor =
+    ActiveRecord::Base.connection.execute("SELECT SUM(labor) FROM sales;").values
     render :index
   end
 end
