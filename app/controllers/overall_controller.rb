1e9ada1162d051964a663dc646df02a4073eb2dc
Nick Ramsey <nramseysc@gmail.com>
2019-10-01 09:37:28 -0700

    returning result from pg object

--- app/controllers/overall_controller.rb
+++ app/controllers/overall_controller.rb
@@ -1,6 +1,8 @@
+require 'pg'
 class OverallController < ApplicationController
+  DB = PG.connect({:dbname => 'tuscan_stone_development'})
   def index
-    @overall_sales = Sale.overall_sales
+    @overall_sales = ActiveRecord::Base.connection.execute("SELECT SUM(sales) FROM sales;").values
     render :index
   end
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,6 +1,5 @@
+require 'pg'
 class Sale < ApplicationRecord
-  scope :overall_sales, -> { where(store_id: "1")}
-
     def self.import(file)
       CSV.foreach(file.path, headers: true) do |row|
         Sale.create! row.to_hash
