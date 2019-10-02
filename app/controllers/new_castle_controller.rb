8e616f9f760352526254d60f37b4eab2ae3eb4e5
Nick Ramsey <nramseysc@gmail.com>
2019-10-02 16:15:55 -0700

    fix-ish most order display

--- app/controllers/new_castle_controller.rb
+++ app/controllers/new_castle_controller.rb
@@ -4,7 +4,7 @@ class NewCastleController < ApplicationController
   def index
     @nc_sales = ActiveRecord::Base.connection.execute("SELECT SUM(nc_sales) FROM masters;").values[0][0]
     @nc_labor = ActiveRecord::Base.connection.execute("SELECT SUM(nc_labor) FROM masters;").values[0][0]
-    @most_orders = ActiveRecord::Base.connection.execute("SELECT products.name, count(orders.id) as orders_count FROM products INNER JOIN orders ON orders.product_id = products.id GROUP BY products.id ORDER BY orders_count DESC LIMIT 3").values[0]
+    @most_orders = ActiveRecord::Base.connection.execute("SELECT products.name, count(orders.id) as orders_count FROM products INNER JOIN orders ON orders.product_id = products.id GROUP BY products.id ORDER BY orders_count DESC LIMIT 3").values()
     render :index
   end
 
