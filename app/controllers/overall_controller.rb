196e893df8daee1d88c92e859058b25cc102a4dd
Nick Ramsey <nramseysc@gmail.com>
2019-10-02 16:21:08 -0700

    route breadcrumbs

--- app/controllers/overall_controller.rb
+++ app/controllers/overall_controller.rb
@@ -4,6 +4,7 @@ class OverallController < ApplicationController
   def index
     @overall_sales = ActiveRecord::Base.connection.execute("SELECT SUM(nc_sales + mi_sales + tm_sales + is_sales) FROM masters;").values[0][0]
     @overall_labor = ActiveRecord::Base.connection.execute("SELECT SUM(nc_labor + mi_labor + tm_labor + is_labor) FROM masters;").values[0][0]
+    @most_orders = ActiveRecord::Base.connection.execute("SELECT products.name, count(orders.id) as orders_count FROM products INNER JOIN orders ON orders.product_id = products.id GROUP BY products.id ORDER BY orders_count DESC LIMIT 3").values()
 
     respond_to do |format|
       format.html
