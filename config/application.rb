ffc9fdcaacd1e76f22860c9a42ae958851a4981f
Nick Ramsey <nramseysc@gmail.com>
2019-10-03 14:12:41 -0700

    really change config file

--- config/application.rb
+++ config/application.rb
@@ -18,12 +18,12 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-config.assets.initialize_on_precompile = false
 Bundler.require(*Rails.groups)
 
 module TuscanStone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.initialize_on_precompile = false
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
